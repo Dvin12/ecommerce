74a19c621d5918cd9f58231dc5a098ad485fe887
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-05-13 18:22:15 +0300

    Created Navbar
    Added Navbar component, basic styling.

---
+++ src/component/Navigation.js
@@ -0,0 +1,34 @@
+import { CiShoppingCart, CiUser } from "react-icons/ci";
+function Navigation() {
+  return (
+    <nav className="bg-slate-200  p-12  w-auto">
+      <section className="flex items-center justify-between mx-28">
+        <h2 className="font-extrabold  text-transparent text-4xl bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 cursor-pointer">
+          LOGO
+        </h2>
+        <ul className="flex gap-10 text-lg tracking-wide text-slate-800 ">
+          <li className="cursor-pointer hover:text-indigo-600 duration-300">
+            Product
+          </li>
+          <li className="cursor-pointer hover:text-indigo-600 duration-300">
+            Explore
+          </li>
+          <li className="cursor-pointer hover:text-indigo-600 duration-300">
+            Support
+          </li>
+          <li className="cursor-pointer hover:text-indigo-600 duration-300">
+            Business
+          </li>
+        </ul>
+        <div>
+          <span className="flex gap-4 text-4xl">
+            <CiShoppingCart className="cursor-pointer"></CiShoppingCart>
+            <CiUser className="cursor-pointer"></CiUser>
+          </span>
+        </div>
+      </section>
+    </nav>
+  );
+}
+
+export default Navigation;
