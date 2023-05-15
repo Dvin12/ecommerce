2cefbc0816ac9214a6b33bf2821998c3f3366b37
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-05-15 22:53:43 +0300

    added dispatch to the component

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,13 +1,40 @@
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { HiArrowNarrowLeft } from "react-icons/hi";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/storeSlice";
 
 function Product() {
+  const dispatch = useDispatch();
   const [details, setDetails] = useState({});
+  let [baseQnty, setBaseQnty] = useState(1);
+
   const location = useLocation();
   useEffect(() => {
     setDetails(location.state.item);
   }, []);
+
+  const handleDecrease = () => {
+    setBaseQnty(baseQnty === 1 ? (baseQnty = 1) : baseQnty - 1);
+  };
+
+  const handleIncrease = () => {
+    setBaseQnty(baseQnty + 1);
+  };
+
+  const handleAddToCart = () => {
+    dispatch(
+      addToCart({
+        id: details.id,
+        name: details.name,
+        brand: details.brand,
+        price: details.price,
+        quantity: baseQnty,
+        description: details.description,
+      })
+    );
+  };
+
   return (
     <div className="bg-slate-200 h-screen">
       <article className="grid grid-cols-2 items-center p-28  w-fit ">
@@ -20,24 +47,27 @@ function Product() {
           <h3 className="text-3xl my-2 font-semibold">${details.price}</h3>
           <p className="my-10 text-lg w-4/5">{details.description}</p>
           <div className="flex gap-12">
-            <div className="border-2 rounded-md border-slate-700 w-[160px] p-2 flex items-center justify-center gap-4">
+            <div className="border-2 rounded-md border-slate-700 w-[180px] p-2 flex items-center justify-center gap-4 ">
               <span>Quantity</span>
-              <button>-</button>
-              <span>1</span>
-              <button>+</button>
+              <button onClick={handleDecrease}>-</button>
+              <span>{baseQnty}</span>
+              <button onClick={handleIncrease}>+</button>
             </div>
-            <button className="bg-indigo-500 w-[160px] rounded-md text-white hover:bg-indigo-600 duration-300 drop-shadow-lg">
+            <button
+              onClick={handleAddToCart}
+              className="bg-indigo-500 w-[180px] rounded-md text-white hover:bg-indigo-600 duration-300 drop-shadow-lg"
+            >
               Add To Cart
             </button>
           </div>
-          <Link to="/">
-            <div className="my-10">
+          <div className="my-8 w-fit">
+            <Link to="/">
               <button className="text-gray-500 font-medium hover:text-gray-800 duration-300 flex items-center">
                 <HiArrowNarrowLeft className="text-lg "></HiArrowNarrowLeft>
                 Go Back
               </button>
-            </div>
-          </Link>
+            </Link>
+          </div>
         </section>
       </article>
     </div>
