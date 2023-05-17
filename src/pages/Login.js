375a1e087e865b2ca79d7ad46ef0d61b7bdda6f2
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-05-17 18:55:52 +0300

    Stylized the Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,24 @@
 import { FcGoogle } from "react-icons/fc";
+import { BsGithub } from "react-icons/bs";
 function Login() {
   return (
     <div className="bg-slate-200  p-12 h-screen">
-      <section className="flex items-center justify-center gap-12 text-xl">
-        <button className="bg-white p-3 w-48 rounded-lg flex items-center justify-center gap-3  drop-shadow-lg">
-          <FcGoogle></FcGoogle>
-          Sign In
-        </button>
-        <button className="bg-gray-700 p-3 w-48 rounded-lg text-white drop-shadow-lg hover:bg-gray-900 duration-300">
-          Sign Out
-        </button>
-      </section>
+      <div className="flex justify-center items-center">
+        <div className="bg-slate-100 p-28 w-fit rounded-2xl drop-shadow-lg my-16 ">
+          <section className="flex flex-col items-center justify-center gap-6 text-xl">
+            <h2 className="font-medium">Login</h2>
+            <button className="bg-white p-4 w-80 rounded-lg flex items-center justify-center gap-3  drop-shadow-lg hover:bg-gray-100 duration-300">
+              <FcGoogle></FcGoogle>
+              Sign In With Google
+            </button>
+            <button className="bg-white p-4 w-80 rounded-lg flex items-center justify-center gap-3  drop-shadow-lg hover:bg-gray-100 duration-300">
+              <BsGithub></BsGithub>
+              Sign in With GitHub
+            </button>
+            <p className="text-xs text-gray-500">For preview purposes only</p>
+          </section>
+        </div>
+      </div>
     </div>
   );
 }
