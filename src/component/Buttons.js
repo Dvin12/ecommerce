97f9bea704222a27a5693dbc8d87f6c0ef065376
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-05-20 16:06:48 +0300

    added styling to the Support page

--- src/component/Buttons.js
+++ src/component/Buttons.js
@@ -7,6 +7,9 @@ function Buttons({ variant, label }) {
   } else if (variant === "learn") {
     buttonClasses +=
       "text-md font-semibold text-gray-600  underline decoration-2 underline-offset-[6px] duration-300 hover:text-gray-900";
+  } else if (variant === "send") {
+    buttonClasses +=
+      "rounded-lg bg-indigo-500 px-4 py-4 text-xl  tracking-wider text-white drop-shadow-lg transition-all duration-300 hover:bg-indigo-600";
   }
 
   return <button className={buttonClasses}>{label}</button>;
--- src/component/ContactForm.js
+++ src/component/ContactForm.js
@@ -1,5 +1,43 @@
+import Buttons from "./Buttons";
+
 function ContactForm() {
-  return <div>Contact Form!</div>;
+  return (
+    <section className="rounded-xl bg-gray-100 drop-shadow-md">
+      <form className="flex flex-col gap-6 p-12">
+        <div>
+          <input
+            placeholder="Full name"
+            className="w-full rounded-md p-3"
+            type="text"
+          ></input>
+        </div>
+        <div>
+          <input
+            placeholder="Email address"
+            className="w-full rounded-md p-3"
+            type="email"
+          ></input>
+        </div>
+        <div>
+          <input
+            placeholder="Subject"
+            className="w-full rounded-md p-3"
+            type="text"
+          ></input>
+        </div>
+        <div>
+          <textarea
+            placeholder="Message"
+            className="h-[100px] w-full resize-none rounded-lg p-3"
+            maxLength="200"
+          ></textarea>
+        </div>
+        <div className="w-fit">
+          <Buttons variant="send" label="Send Message"></Buttons>
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default ContactForm;
