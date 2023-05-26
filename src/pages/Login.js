7be805d687a6ad4399c9e16414ed472de6986055
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-05-26 12:10:45 +0300

    adjustments

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,7 @@ import { FcGoogle } from "react-icons/fc";
 import { BsGithub } from "react-icons/bs";
 function Login() {
   return (
-    <main className="h-screen bg-slate-200  p-12 md:h-screen xl:h-full">
+    <main className="h-screen bg-slate-200  p-12  xl:h-full">
       <div className="flex h-full items-center justify-center">
         <div className="mx-auto  drop-shadow-lg xl:my-10 xl:rounded-2xl xl:bg-slate-100 xl:p-28">
           <section className="flex flex-col items-center justify-center gap-6 text-xl">
